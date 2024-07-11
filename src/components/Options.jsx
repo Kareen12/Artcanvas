aec7f20fe0f25c5bd07b33b70b0dd18b0ae7376a
Kareen Ujjainiya <kareenujjainiya222@gmail.com>
2024-07-12 00:04:51 +0530

    Initial commit

---
+++ src/components/Options.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "./Styles";
+
+const Options = (prop) => {
+  return (
+    <div>
+      <Button onClick={prop.method_props.clearCanvas_prop}>Clear</Button>
+      <Button onClick={prop.method_props.redo_props}>Redo</Button>
+      <Button onClick={prop.method_props.erase_prop}>Eraser</Button>
+      <Button onClick={prop.method_props.undo_props}>Undo</Button>
+      <Button onClick={prop.method_props.saveImage_prop}>Save</Button>
+    </div>
+  );
+};
+
+export default Options;
---
+++ src/components/Styles.jsx
@@ -0,0 +1,61 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const StyledCanvas = styled.canvas`
+  box-shadow: 8px 10px 108px rgb(39 123 145);
+  border: 3px solid #467896;
+  resize: vertical;
+  margin-top: 30px;
+  border-radius: 20px;
+  &:active {
+    cursor: pointer;
+  }
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const ToolsDiv = styled(StyledDiv)`
+  background-color: black;
+  padding: 10px 100px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px;
+`;
+export const Div = styled(ToolsDiv)`
+  display: flex;
+  justify-content: space-between;
+  /* background-color: transparent; */
+`;
+export const StyledInput = styled.input`
+  padding: ${(prop) =>
+    prop.type === "color" ? "0px 10px" : "2px 10px 2px 10px"};
+  margin: 0px 20px;
+  background-color: rgb(39 123 145);
+  color: white;
+`;
+export const GlobalStyle = createGlobalStyle`
+body{
+    background: black;
+    padding: 0px;
+    margin: 0px;
+}
+`;
+
+export const Button = styled.button`
+  margin: 0px 5px 0px 5px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: rgb(39 123 145);
+  color: white;
+
+  &:hover {
+    transition: color 2s, background-color 2s, text-transform 3s;
+    color: white;
+    background-color: black;
+    text-transform: uppercase;
+    box-shadow: 1px 1px 5px rgb(30 100 150);
+  }
+`;
